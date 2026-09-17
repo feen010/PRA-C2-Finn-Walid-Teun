a66f6ad028ebc9f9e7b0ec1e749cf97e3df1cd07
zkhb9m957z-byte <zkhb9m957z@privaterelay.appleid.com>
2026-09-17 12:57:53 +0200

    ticket nr 485 gemaakt

--- includes/collegas/ine.php
+++ includes/collegas/ine.php
@@ -2,5 +2,6 @@
 	<img src="img/profile_pictures/ib81.jfif" alt="ib81">
 	<h4>Ine Vermeer </h4>
 	<p>docent WEB/SLB</p>
+	<p>t/m april met verlof</p>
 	<p><a href="mailto:e.vermeer@curio.nl">e.vermeer@curio.nl</a></p>
 </div>
